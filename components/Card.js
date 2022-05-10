550b136fb509c3370f43166e49d3d5b6ebcb1430
jacob-lcs <jacob.lcs@qq.com>
2022-05-10 16:33:05 +0800

    feat: add projects

--- components/Card.js
+++ components/Card.js
@@ -45,7 +45,7 @@ const Card = ({ title, description, imgSrc, href }) => (
             className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label={`Link to ${title}`}
           >
-            Learn more &rarr;
+            查看更多 &rarr;
           </Link>
         )}
       </div>
--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,23 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: '课程系统',
+    description: `自己在大学的时候做了一个课程管理与推荐系统，系统的功能包括课程表、课程推荐、课程群聊三大功能。
+    其实自己也是在大学时候感受到没有一款相似的工具出现，并且平时想找大学课程班上的同学比较困难，因为大学是走班制，没有固定的班级。
+    所以就做了这么一款软件。总体来说分为 Android 端、Web 端、Nodejs 端。`,
+    imgSrc: '/static/images/projects/schedule.png',
+    href: 'http://schedule.lcs.show/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '富文本编辑器',
+    description: `做了一个比较简单的富文本编辑器，能够实现加粗、斜体、下划线等基础功能`,
+    imgSrc: '/static/images/projects/editor.png',
+    href: 'http://editor.lcs.show/',
+  },
+  {
+    title: '开心消消乐',
+    description: `一个比较简单的消消乐游戏`,
+    imgSrc: '/static/images/projects/xxle.png',
+    href: 'http://xxle.lcs.show/',
   },
 ]
 
