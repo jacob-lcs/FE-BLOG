e47cbdc3e6cc4190b7793a81736789593e51dbbd
Jacob <jacob.lcs@qq.com>
2022-07-17 02:05:57 +0000

    chore: upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -58,6 +58,7 @@ module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
   },
+  swcMinify: true,
   async headers() {
     return [
       {
