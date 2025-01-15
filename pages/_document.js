a37904705dcb06fe05b187c569ea3b43e3f7af46
Jacob <jacob.lcs@qq.com>
2025-01-15 22:38:10 +0800

    feat: add baidu

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,7 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/static/favicons/favicon-16x16.png"
           />
+          <meta name="baidu-site-verification" content="codeva-pYAQU8OXxE" />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#000000" />
