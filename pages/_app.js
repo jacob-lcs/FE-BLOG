ce45e872a6a7bd1fc2a7a9ec5c6925790edc5a9f
Jacob <jacob.lcs@qq.com>
2025-01-15 22:52:14 +0800

    chore: remove @vercel/analytics dependency from package.json and related files

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,6 @@ import '@fontsource/inter/variable-full.css'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
-import { Analytics } from '@vercel/analytics/react'
 
 import siteMetadata from '@/data/siteMetadata'
 // import Analytics from '@/components/analytics'
@@ -22,7 +21,6 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
-      <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
