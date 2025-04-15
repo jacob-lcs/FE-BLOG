2df25631b43cccf2d7ab0032109aea9c87056f55
jacob-lcs <jacob.lcs@qq.com>
2025-04-15 15:49:26 +0800

    fix: update project links to point to GitHub repositories

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,13 +5,13 @@ const projectsData = [
     其实自己也是在大学时候感受到没有一款相似的工具出现，并且平时想找大学课程班上的同学比较困难，因为大学是走班制，没有固定的班级。
     所以就做了这么一款软件。总体来说分为 Android 端、Web 端、Nodejs 端。`,
     imgSrc: '/static/images/projects/schedule.png',
-    href: 'http://schedule.lcs.show/',
+    href: 'https://github.com/jacob-lcs/awesome-curriculum-web',
   },
   {
     title: '富文本编辑器',
     description: `做了一个比较简单的富文本编辑器，能够实现加粗、斜体、下划线等基础功能`,
     imgSrc: '/static/images/projects/editor.png',
-    href: 'http://editor.lcs.show/',
+    href: 'https://github.com/jacob-lcs/AEditor',
   },
   {
     title: '开心消消乐',
